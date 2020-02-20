8f3d9f621eb4d9e05ae666f05a61293ed5af71bd
Rosklin Juliano Chagas <rosklin@Rosklins-MacBook-Air.local>
2020-02-20 09:32:17 -0300

    pagina php

--- pagina.php
+++ pagina.php
@@ -250,6 +250,30 @@
 			echo "v() * 2 = " . $v[$i] . "<br>";
 		}
 
+		// Função para indicar se um número é primo.
+		function primo($n) {
+			
+		}
+
+		// Função para retornar os números pares de um vetor.
+		function pares($v) {
+
+		}
+
+		// Função para verificar se um vetor está ordenado.
+		// Exemplo: [2, 2, 3] (V)   [2, 1, 4] (F).
+		function ordenado($v) {
+
+		}
+
+
+		$vetor = array(3, 5, 8, 7);
+
+		$p = pares($vetor);
+
+		for($i=0; $i<count($p); $i++) {
+			echo "par: " . $p[$i] . "<br>";
+		}
 
 	?>
 
